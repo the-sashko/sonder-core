396ae20a7c13d961945841a0e5a15dd8e372ea26
The Sashko <sashko.me>
2019-02-28 18:25:16 +0200

    refactoring routing

--- init.php
+++ init.php
@@ -3,27 +3,48 @@ if (!defined('APP_MODE')) {
     define('APP_MODE', 'default');
 }
 
+if (file_exists(__DIR__.'/../routers/default.php')) {
+    $routerPath = __DIR__.'/../routers/default.php';
+} else {
+    $routerPath = __DIR__.'/examples/router.php';
+}
+
+
 switch (APP_MODE) {
     case 'dev':
         require_once __DIR__.'/config/dev.php';
+        require_once $routerPath;
+        require_once __DIR__.'/app.php';
+        (new App)->run();
         break;
     
     case 'prod':
         require_once __DIR__.'/config/prod.php';
+        require_once $routerPath;
+        require_once __DIR__.'/app.php';
+        (new App)->run();
         break;
 
     case 'test':
         require_once __DIR__.'/config/test.php';
+        require_once $routerPath;
+        require_once __DIR__.'/app.php';
+        (new App)->run();
+        break;
+
+    case 'api':
+        require_once __DIR__.'/config/api.php';
+        require_once $routerPath;
+        require_once __DIR__.'/api.php';
+        (new API)->run();
+        break;
+
+    case 'default':
+        require_once __DIR__.'/config/prod.php';
+        require_once $routerPath;
+        require_once __DIR__.'/app.php';
+        (new App)->run();
         break;
 }
-
-if (file_exists(__DIR__.'/../router.php')) {
-    require_once __DIR__.'/../router.php';
-} else {
-    require_once __DIR__.'/examples/router.php';
-}
-
-require_once __DIR__.'/app.php';
-(new App)->run();
 
 ?>
