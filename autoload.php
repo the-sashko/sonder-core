16049e8a35f33a4996a4c133121890a8fb3ef0d6
The Sashko <inbox@sashko.me>
2019-05-13 18:30:57 +0300

    add authinterface

--- autoload.php
+++ autoload.php
@@ -5,6 +5,7 @@ require_once __DIR__.'/classes/common.class.php';
 require_once __DIR__.'/classes/model.object.class.php';
 require_once __DIR__.'/classes/model.class.php';
 require_once __DIR__.'/classes/model.crud.interface.php';
+require_once __DIR__.'/classes/model.auth.interface.php';
 require_once __DIR__.'/classes/controller.class.php';
 require_once __DIR__.'/classes/controller.cron.class.php';
 ?>
--- classes/values.object.php
+++ classes/values.object.php
@@ -5,7 +5,7 @@
 class ValuesObject
 {
     /**
-     * @var List Of Model Instance Data
+     * @var array List Of Model Instance Data
      */
     public $data = [];
 
