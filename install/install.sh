3376131483c59d7aed49432799f1a1f393d25975
The Sashko <sashko.me>
2019-07-01 17:37:12 +0300

    fix install script

--- install/install.sh
+++ install/install.sh
@@ -3,7 +3,7 @@
 currDir=$(pwd)
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
-cd "$scriptDir/.."
+cd "$scriptDir/../.."
 
 cp -r core/examples/public ../public
 cp -r core/examples/protected/config config
