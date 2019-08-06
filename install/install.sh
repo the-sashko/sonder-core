8787df64ed33df8e44644c292879ae4beb5af37f
The Sashko <18634500+the-sashko@users.noreply.github.com>
2019-08-06 12:31:49 +0300

    fix install script

--- install/install.sh
+++ install/install.sh
@@ -5,6 +5,7 @@ scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
 
 cd "$scriptDir/../.."
 
+cp core/examples/.gitignore .gitignore
 cp -r core/examples/public ../public
 cp -r core/examples/protected/config config
 cp -r core/examples/protected/controllers controllers
