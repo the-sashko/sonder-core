a9ab035e4008c3143182e3ecba7a14809a9b2bc4
The Sashko <sashko.me>
2020-10-21 15:20:37 +0300

    improve install

--- install/install.sh
+++ install/install.sh
@@ -14,6 +14,7 @@ cp -r core/install/samples/protected/models models
 cp -r core/install/samples/protected/hooks hooks
 cp -r core/install/samples/protected/res res
 cp -r core/install/samples/protected/routers routers
+cp -r core/install/samples/protected/tests tests
 cp -r core/install/samples/protected/init.php init.php
 cp -r core/install/samples/protected/api.init.php api.init.php
 cp -r core/install/samples/protected/cli.init.php cli.init.php
---
+++ install/samples/protected/tests/.phan/config.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'target_php_version' => '7.2',
+
+    'directory_list' => [
+        '../'
+    ],
+
+    'exclude_analysis_directory_list' => [
+        '../tests'
+    ],
+];
