df7124eea6cf604e9c811ff875e6565a3a1053ff
The Sashko <sashko.me>
2020-10-21 13:45:01 +0300

    add phan tests config

---
+++ tests/.phan/config.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+    'target_php_version' => '7.2',
+
+    'directory_list' => [
+    	//'../plugins/breadcrumbs',
+        '../plugins/captcha',
+        '../plugins/crypt',
+        '../plugins/database',
+        //'../plugins/error',
+        '../plugins/geoip',
+        '../plugins/image',
+        //'../plugins/language',
+        //'../plugins/link',
+        '../plugins/logger',
+        //'../plugins/mail',
+        //'../plugins/markup',
+        //'../plugins/math',
+        '../plugins/mock',
+        //'../plugins/page',
+        //'../plugins/paginator',
+        //'../plugins/push',
+        //'../plugins/qr',
+        //'../plugins/redis',
+        '../plugins/security',
+        //'../plugins/serverInfo',
+        '../plugins/session',
+        //'../plugins/share',
+        //'../plugins/shortener',
+        //'../plugins/sitemap',
+        //'../plugins/sms',
+        //'../plugins/templater',
+        //'../plugins/translit',
+        '../plugins/upload',
+        //'../plugins/validator',
+        //'../plugins/youtube',
+        //'../plugins/'
+    ],
+
+    'exclude_analysis_directory_list' => [
+        '../tests',
+        '../plugins/captcha/tests/',
+        '../plugins/captcha/res/examples/'
+    ],
+];
