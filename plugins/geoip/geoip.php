7183ae124e7c9b3b8d0d993ffbc624866177a8e4
The Sashko <sashko.me>
2020-10-21 13:48:54 +0300

    improve geoip plugin

--- plugins/geoip/geoip.php
+++ plugins/geoip/geoip.php
@@ -4,44 +4,65 @@
  */
 class GeoIPPlugin
 {
+    const DEFAULT_IP = '0.0.0.0';
+
     /**
      * Get User IP Address From Request
      *
      * @return string IP Address
      */
-    public function getIP() : string
+    public function getIP(): string
     {
-        $serverInfo = new ServerInfo();
-
-        if ($serverInfo->has('HTTP_CF_CONNECTING_IP')) {
-            return $serverInfo->get('HTTP_CF_CONNECTING_IP');
+        if (
+            array_key_exists('HTTP_CF_CONNECTING_IP', $_SERVER) &&
+            !empty($_SERVER['HTTP_CF_CONNECTING_IP'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_CF_CONNECTING_IP']);
         }
 
-        if ($serverInfo->has('HTTP_CLIENT_IP')) {
-            return $serverInfo->get('HTTP_CLIENT_IP');
+        if (
+            array_key_exists('HTTP_CLIENT_IP', $_SERVER) &&
+            !empty($_SERVER['HTTP_CLIENT_IP'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_CLIENT_IP']);
         }
 
-        if ($serverInfo->has('HTTP_X_FORWARDED_FOR')) {
-            return $serverInfo->get('HTTP_X_FORWARDED_FOR');
+        if (
+            array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) &&
+            !empty($_SERVER['HTTP_X_FORWARDED_FOR'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_X_FORWARDED_FOR']);
         }
 
-        if ($serverInfo->has('HTTP_X_FORWARDED')) {
-            return $serverInfo->get('HTTP_X_FORWARDED');
+        if (
+            array_key_exists('HTTP_X_FORWARDED', $_SERVER) &&
+            !empty($_SERVER['HTTP_X_FORWARDED'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_X_FORWARDED']);
         }
 
-        if ($serverInfo->has('HTTP_FORWARDED_FOR')) {
-            return $serverInfo->get('HTTP_FORWARDED_FOR');
+        if (
+            array_key_exists('HTTP_FORWARDED_FOR', $_SERVER) &&
+            !empty($_SERVER['HTTP_FORWARDED_FOR'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_FORWARDED_FOR']);
         }
 
-        if ($serverInfo->has('HTTP_FORWARDED')) {
-            return $serverInfo->get('HTTP_FORWARDED');
+        if (
+            array_key_exists('HTTP_FORWARDED', $_SERVER) &&
+            !empty($_SERVER['HTTP_FORWARDED'])
+        ) {
+            return $this->_escapeInput($_SERVER['HTTP_FORWARDED']);
         }
 
-        if ($serverInfo->has('REMOTE_ADDR')) {
-            return $serverInfo->get('REMOTE_ADDR');
+        if (
+            array_key_exists('REMOTE_ADDR', $_SERVER) &&
+            !empty($_SERVER['REMOTE_ADDR'])
+        ) {
+            return $this->_escapeInput($_SERVER['REMOTE_ADDR']);
         }
 
-        return '0.0.0.0';
+        return static::DEFAULT_IP;
     }
 
     /**
@@ -49,9 +70,23 @@ class GeoIPPlugin
      *
      * @return string IP Address
      */
-    public function getIPHash() : string
+    public function getIPHash(): string
     {
         return hash('sha256', $this->getIP());
     }
+
+    public function _escapeInput(?string $inputString = null): string
+    {
+        $inputString = (string) $inputString;
+        $inputString = strip_tags($inputString);
+        $inputString = htmlspecialchars($inputString);
+        $inputString = addslashes($inputString);
+        $inputString = preg_replace('/\s/su', '', $inputString);
+
+        if (empty($inputString)) {
+            $inputString = static::DEFAULT_IP;
+        }
+
+        return $inputString;
+    }
 }
-?>
