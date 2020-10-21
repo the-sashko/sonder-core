8eb3efaab02c7a86aa7b32ce092190de373f5461
The Sashko <sashko.me>
2020-10-21 15:20:12 +0300

    improve phan config

--- tests/.phan/config.php
+++ tests/.phan/config.php
@@ -4,43 +4,17 @@ return [
     'target_php_version' => '7.2',
 
     'directory_list' => [
-    	//'../plugins/breadcrumbs',
-        '../plugins/captcha',
-        '../plugins/crypt',
-        '../plugins/database',
-        //'../plugins/error',
-        '../plugins/geoip',
-        '../plugins/image',
-        //'../plugins/language',
-        //'../plugins/link',
-        '../plugins/logger',
-        //'../plugins/mail',
-        //'../plugins/markup',
-        //'../plugins/math',
-        '../plugins/mock',
-        //'../plugins/page',
-        //'../plugins/paginator',
-        //'../plugins/push',
-        //'../plugins/qr',
-        //'../plugins/redis',
-        '../plugins/security',
-        //'../plugins/serverInfo',
-        '../plugins/session',
-        //'../plugins/share',
-        //'../plugins/shortener',
-        //'../plugins/sitemap',
-        //'../plugins/sms',
-        //'../plugins/templater',
-        //'../plugins/translit',
-        '../plugins/upload',
-        //'../plugins/validator',
-        //'../plugins/youtube',
-        //'../plugins/'
+        '../'
     ],
 
     'exclude_analysis_directory_list' => [
         '../tests',
         '../plugins/captcha/tests/',
-        '../plugins/captcha/res/examples/'
+        '../plugins/captcha/res/examples/',
+        '../plugins/language/vendor',
+        '../plugins/qr/vendor',
+        '../plugins/redis/vendor',
+        '../plugins/mail/providers/SMTP/vendor',
+        '../plugins/share/twitter/vendor'
     ],
 ];
