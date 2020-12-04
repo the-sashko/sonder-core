169fa24215f4f82a8cabb4521846469685f71037
The Sashko <sashko.me>
2020-12-04 18:24:33 +0200

    add 2 empty tests

---
+++ tests/plugins/DatabasePluginTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class For Testing DatabasePlugin Class Methods
+ */
+class DatabasePluginTest extends TestCase
+{
+    public function testTest()
+    {
+        // To-Do
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/plugins/SmsPluginTest.php
@@ -0,0 +1,15 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class For Testing SmsPlugin Class Methods
+ */
+class SmsPluginTest extends TestCase
+{
+    public function testTest(): void
+    {
+        // To-Do
+
+        $this->assertTrue(true);
+    }
+}
