fc420324c45ab82690b330ba6a556619ff90d0f3
The Sashko <sashko.me>
2021-04-02 17:54:13 +0300

    refactoring

--- install/public/assets/js/example.js
+++ install/public/assets/js/example.js
@@ -2,9 +2,11 @@
  * @file Example JS Application
  */
 let example = {
-    'init': function() {
-        console.log('Hello World!');
+
+    "init": function () {
+        console.log("Hello World!");
     }
+
 };
 
 example.init();
