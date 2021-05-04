b7ceed38c72bc6387db48e1368513009baf97c85
The Sashko <18634500+the-sashko@users.noreply.github.com>
2021-05-05 02:03:15 +0300

    Update test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,7 +2,7 @@
 export PHAN_ALLOW_XDEBUG=1
 export PHAN_DISABLE_XDEBUG_WARN=1
 
-currDir=$(pwd)
+currentDir=$(pwd)
 scriptDir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
 
 cd "$scriptDir/../tests" || exit 1
@@ -15,7 +15,7 @@ cd ../plugins/captcha/tests || exit 1
 phpunit
 php ../../../tests/tools/phan.phar
 
-cd "$currDir" || exit 1
+cd "$currentDir" || exit 1
 
 exit 1
 
