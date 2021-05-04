3646b445d054cc23ad8a55b5df4ee8341d1b31fa
The Sashko <sashko.me>
2021-05-05 01:50:57 +0300

    refactoring autoload plugins

--- plugins/autoload.php
+++ plugins/autoload.php
@@ -1,75 +1,27 @@
 <?php
 class PluginsAutoload
 {
-    public $loadAll     = true;
-    public $configFile  = __DIR__.'/../../config/plugins.json';
-    public $list        = [];
-
-    public function init() : bool
+    public function load(): void
     {
-        $this->_initConfig();
+        array_map([$this, '_loadPlugin'], glob(__DIR__.'/*', GLOB_ONLYDIR));
+    }
 
-        if ($this->loadAll) {
-            $this->_loadAll();
-
+    private function _loadPlugin(?string $pluginDir = null): bool
+    {
+        if (empty($pluginDir)) {
             return false;
         }
 
-        $this->_loadListed();
+        $pluginInitPath = "{$pluginDir}/init.php";
 
-        return true;
-    }
-
-    private function _loadAll() : void
-    {
-        foreach (glob(__DIR__.'/*', GLOB_ONLYDIR) as $pluginDir) {
-            $this->_loadPlugin($pluginDir);
-        }
-    }
-
-    private function _loadListed() : void
-    {
-        foreach ($this->list as $pluginName) {
-            $pluginName = (string) $pluginName;
-            $pluginName = mb_convert_case(trim($pluginName), MB_CASE_LOWER);
-
-            $this->_loadPlugin(__DIR__.'/'.$pluginName);
-        }
-    }
-
-    private function _loadPlugin(string $pluginDir = '') : void
-    {
-        if (
-            file_exists("{$pluginDir}/init.php") &&
-            is_file("{$pluginDir}/init.php")
-        ) {
-            require_once "{$pluginDir}/init.php";
-        }
-    }
-
-    private function _initConfig(): bool
-    {
-        if (!file_exists($this->configFile) || !is_file($this->configFile)) {
+        if (!file_exists($pluginInitPath) || !is_file($pluginInitPath)) {
             return false;
         }
 
-        $config = file_get_contents($this->configFile);
-        $config = json_decode($config, true);
-
-        if (!array_key_exists('mode', $config) || $config['mode'] === 'all') {
-            return false;
-        }
-
-        $this->loadAll = false;
-
-        if (!array_key_exists('list', $config) || is_array($config['list'])) {
-            return false;
-        }
-
-        $this->list = $config['list'];
+        require_once $pluginInitPath;
 
         return true;
     }
 }
 
-(new PluginsAutoload)->init();
+(new PluginsAutoload)->load();
