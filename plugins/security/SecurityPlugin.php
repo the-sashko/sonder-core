3dcf2db25939300c298aacb2104a970f250db2a3
The Sashko <inbox@sashko.me>
2021-10-12 00:58:37 +0300

    add plugins

---
+++ plugins/security/SecurityPlugin.php
@@ -0,0 +1,25 @@
+<?php
+namespace Sonder\Plugins;
+
+final class SecurityPlugin
+{
+    final public function escapeInput(mixed $input = null): mixed
+    {
+        if (is_array($input)) {
+            return array_map([$this, 'escapeInput'], $input);
+        }
+
+        if (empty($input)) {
+            return null;
+        }
+
+        if (!is_string($input)) {
+            return $input;
+        }
+
+        $input = strip_tags($input);
+        $input = htmlspecialchars($input);
+
+        return addslashes($input);
+    }
+}
