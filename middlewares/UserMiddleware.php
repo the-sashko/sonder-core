2b704048a77de9690e607aa96364f16e112917c6
The Sashko <inbox@sashko.me>
2022-01-08 20:36:04 +0200

    improve user middleware

--- middlewares/UserMiddleware.php
+++ middlewares/UserMiddleware.php
@@ -13,6 +13,13 @@ final class UserMiddleware extends CoreMiddleware implements IMiddleware
      */
     final public function run(): void
     {
+        if (empty($this->request->getSession())) {
+            $errorMessage = 'User Middleware Must Be Run After Session ' .
+                'Middleware';
+
+            throw new Exception($errorMessage);
+        }
+
         $this->request->setUser($this->getModel('user'));
     }
 }
