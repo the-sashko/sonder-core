0fd11a6a30b6f2074f97e34d40a97d521590f8be
The Sashko <inbox@sashko.me>
2022-02-09 04:03:16 +0200

    add core cron interfaces

---
+++ core/interfaces/cron.interface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sonder\Core\Interfaces;
+
+interface ICron
+{
+    //TODO
+}
---
+++ core/interfaces/cron.vo.interface.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Sonder\Core\Interfaces;
+
+interface ICronValuesObject
+{
+    //TODO
+}
