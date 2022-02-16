b0e7dd192bf5a24a7cf7b8413896018329ce1dbe
The Sashko <inbox@sashko.me>
2022-02-16 06:08:32 +0200

    improve cron controller

--- controllers/CronController.php
+++ controllers/CronController.php
@@ -2,12 +2,69 @@
 
 namespace Sonder\Controllers;
 
+use Exception;
 use Sonder\Core\CoreController;
 use Sonder\Core\Interfaces\IController;
+use Sonder\Core\ResponseObject;
+use Sonder\Models\Cron;
+use Sonder\Models\Cron\CronValuesObject;
+use Sonder\Plugins\Database\Exceptions\DatabaseCacheException;
+use Sonder\Plugins\Database\Exceptions\DatabasePluginException;
 
 final class CronController extends CoreController implements IController
 {
-    final public function displayRun()
+    /**
+     * @no_cache true
+     *
+     * @return ResponseObject
+     * @throws DatabaseCacheException
+     * @throws DatabasePluginException
+     * @throws Exception
+     */
+    final public function displayRun(): ResponseObject
+    {
+        /* @var $cronModel Cron */
+        $cronModel = $this->getModel('cron');
+
+        /* @var $cronJobs CronValuesObject[] */
+        $cronJobs = $cronModel->getJobsForRunning();
+
+        if (empty($cronJobs)) {
+            $this->response->setContent("No Cron Jobs For Running\n");
+
+            return $this->response;
+        }
+
+        $successfulJobsCount = 0;
+        $failedJobsCount = 0;
+
+        foreach ($cronJobs as $cronVO) {
+            $jobStatus = $cronModel->runJob($cronVO);
+
+            if ($jobStatus) {
+                $successfulJobsCount++;
+            }
+
+            if (!$jobStatus) {
+                $failedJobsCount++;
+            }
+        }
+
+        $this->response->setContent(sprintf(
+            "Successful Jobs: %d\nFailed Jobs: %d\n",
+            $successfulJobsCount,
+            $failedJobsCount
+        ));
+
+        return $this->response;
+    }
+
+    final public function jobRouter(): void
+    {
+        //TODO
+    }
+
+    final public function jobTranslations(): void
     {
         //TODO
     }
